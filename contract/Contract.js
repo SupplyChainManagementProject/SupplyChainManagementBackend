4bd4dcbef5b32da89c1bf13d05dd8b7988e27559
Shagil Islam <shagilislam786@gmail.com>
2023-04-11 20:09:13 +0530

    Sepolia Testnet Added

--- contract/Contract.js
+++ contract/Contract.js
@@ -2,12 +2,12 @@ const { ethers, providers } = require('ethers');
 const dotenv = require('dotenv')
 dotenv.config()
 
-const provider = new ethers.providers.JsonRpcProvider(String(process.env.RPC_ENDPOINT));
-const privateKey = process.env.PRIVATE_KEY; 
+const provider = new ethers.providers.JsonRpcProvider(String(process.env.SEPOLIA_RPC_URL));
+const privateKey = process.env.SEPOLIA_PRIVATE_KEY; 
 const contractAbi = require('../contract/MainContractABI.json')
 
 const signer = new ethers.Wallet(privateKey, provider);
-const contractAddress = process.env.CONTRACT_ADDRESS;
+const contractAddress = process.env.SEPOLIA_CONTRACT_ADDRESS;
 const contract = new ethers.Contract(contractAddress, contractAbi, signer);
 
 module.exports = contract
