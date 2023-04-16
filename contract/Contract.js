a0c2d35f299c0b38c1623db5a17f6ca806845878
Shagil Islam <shagilislam786@gmail.com>
2023-04-16 21:42:24 +0530

    Switched back to Sepolia testnet

--- contract/Contract.js
+++ contract/Contract.js
@@ -3,11 +3,11 @@ const dotenv = require('dotenv')
 dotenv.config()
 
 const provider = new ethers.providers.JsonRpcProvider(String(process.env.SEPOLIA_RPC_URL));
-const privateKey = process.env.POLYGON_PRIVATE_KEY; 
+const privateKey = process.env.SEPOLIA_PRIVATE_KEY; 
 const contractAbi = require('../contract/MainContractABI.json')
 
 const signer = new ethers.Wallet(privateKey, provider);
-const contractAddress = process.env.POLYGON_CONTRACT_ADDRESS;
+const contractAddress = process.env.SEPOLIA_CONTRACT_ADDRESS;
 const contract = new ethers.Contract(contractAddress, contractAbi, signer);
 
 module.exports = contract
